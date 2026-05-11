7afc8d595e9b4039c1463388e87dc9e0543da456
mehdi munim <mehdi.munim@gmail.com>
2026-05-11 17:54:46 +0200

    chore: edit videos

--- data/videos.js
+++ data/videos.js
@@ -2,20 +2,20 @@
 const videoData = [
   { 
     id: 1, 
-    titre_question: "Interview 1 ?", 
+    titre_question: "Interview 1", 
     url_video: "https://www.w3schools.com/html/mov_bbb.mp4", 
     url_miniature: "https://picsum.photos/seed/ia/400/250" 
   },
   { 
     id: 2, 
-    titre_question: "Interview 2 ?", 
-    url_video: "https://www.w3schools.com/html/mov_bbb.mp4", 
+    titre_question: "Interview 2", 
+    url_video: "https://media.w3.org/2010/05/sintel/trailer.mp4", 
     url_miniature: "https://picsum.photos/seed/climat/400/250" 
   },
   { 
     id: 3, 
-    titre_question: "Interview 3 ?", 
-    url_video: "https://www.w3schools.com/html/mov_bbb.mp4", 
+    titre_question: "Interview 3", 
+    url_video: "https://media.w3.org/2010/05/bunny/trailer.mp4", 
     url_miniature: "https://picsum.photos/seed/remote/400/250" 
   }
 ];
