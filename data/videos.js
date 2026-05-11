7f38a215dbbe5e6cc4c3e9625992f3226921cc4f
mehdi munim <mehdi.munim@gmail.com>
2026-05-11 17:51:01 +0200

    chore: modified titles

--- data/videos.js
+++ data/videos.js
@@ -2,19 +2,19 @@
 const videoData = [
   { 
     id: 1, 
-    titre_question: "Que pensez-vous de l'IA ?", 
+    titre_question: "Interview 1 ?", 
     url_video: "https://www.w3schools.com/html/mov_bbb.mp4", 
     url_miniature: "https://picsum.photos/seed/ia/400/250" 
   },
   { 
     id: 2, 
-    titre_question: "Votre avis sur le climat ?", 
+    titre_question: "Interview 2 ?", 
     url_video: "https://www.w3schools.com/html/mov_bbb.mp4", 
     url_miniature: "https://picsum.photos/seed/climat/400/250" 
   },
   { 
     id: 3, 
-    titre_question: "Un mot sur le télétravail ?", 
+    titre_question: "Interview 3 ?", 
     url_video: "https://www.w3schools.com/html/mov_bbb.mp4", 
     url_miniature: "https://picsum.photos/seed/remote/400/250" 
   }
