8b79662083ba5011c083fa882418459d933054cc
Mehdi Munim <mehdi.munim@gmail.com>
2026-05-18 15:15:27 +0200

    update micro trottoir

--- data/videos.js
+++ data/videos.js
@@ -8,14 +8,14 @@ const videoData = [
   },
 
   {id : 2,
-    titre_question : "IA vs. chercheur.euse"
-    url_video : "https://video.univ-rennes.fr/permalink/v126d44fb9c10k5dviyx/iframe/"
+    titre_question : "IA vs. chercheur.euse",
+    url_video : "https://video.univ-rennes.fr/permalink/v126d44fb9c10k5dviyx/iframe/",
     url_miniature : "https://picsum.photos/seed/rennes/400/250"
   }
   
   {id : 3,
-    titre_question : "IA et législation"
-    url_video : "https://video.univ-rennes.fr/permalink/v126d44fb9ca2m85i6ka/iframe/ "
+    titre_question : "IA et législation",
+    url_video : "https://video.univ-rennes.fr/permalink/v126d44fb9ca2m85i6ka/iframe/",
     url_miniature : "https://picsum.photos/seed/rennes/400/250"
   }
 
