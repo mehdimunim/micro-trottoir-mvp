4f716c9b96c7bb555d298d32cfb6a1db17f10e6d
Mehdi Munim <mehdi.munim@gmail.com>
2026-05-18 15:52:20 +0200

    chore: add thumbnails

--- data/videos.js
+++ data/videos.js
@@ -4,20 +4,34 @@ const videoData = [
     id: 1, 
     titre_question: "IA vs chercheur.euse", 
     url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9c10k5dviyx/iframe/ ", 
-    url_miniature: "https://picsum.photos/seed/rennes/400/250" 
+    url_miniature: "assets/img/IA_et_recherche.png" 
   },
 
   { 
     id: 2, 
     titre_question: "IA et législation", 
     url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9ca2m85i6ka/iframe/ ", 
-    url_miniature: "https://picsum.photos/seed/rennes/400/250" 
+    url_miniature: "assets/img/IA_et_droit.png" 
   },
 
   { 
     id: 3, 
     titre_question: "IA et enseignement", 
     url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9328lzfp40f/iframe/", 
-    url_miniature: "https://picsum.photos/seed/rennes/400/250" 
+    url_miniature: "assets/img/IA_enseignement.png" 
+  },
+
+  { 
+    id: 4, 
+    titre_question: "IA et humain", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d44fc8253dl70bzg/iframe//", 
+    url_miniature: "assets/img/IA.png" 
+  },
+
+  { 
+    id: 4, 
+    titre_question: "IA et environnement", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d44fc8253dl70bzg/iframe//", 
+    url_miniature: "assets/img/IA_et_environnement.png" 
   },
 ];
