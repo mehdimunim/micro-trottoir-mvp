620ea2a73923a01fd79011c1fc89ae937587845d
Mehdi Munim <mehdi.munim@gmail.com>
2026-05-18 15:40:22 +0200

    feat: Add videos

--- data/videos.js
+++ data/videos.js
@@ -4,19 +4,27 @@ const videoData = [
     id: 1, 
     titre_question: "Capsule Vidéo Rennes", 
     url_video: "https://video.univ-rennes.fr/permalink/v126d44a11d03izwlvoz/iframe/", 
+    url_miniature: "https://canva.link/4p1mwtgb2uld31h" 
+  },
+
+  { 
+    id: 2, 
+    titre_question: "IA vs chercheur.euse", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9c10k5dviyx/iframe/ ", 
     url_miniature: "https://picsum.photos/seed/rennes/400/250" 
   },
 
-  {id : 2,
-    titre_question : "IA vs. chercheur.euse",
-    url_video : "https://video.univ-rennes.fr/permalink/v126d44fb9c10k5dviyx/iframe/",
-    url_miniature : "https://picsum.photos/seed/rennes/400/250"
-  }
-  
-  {id : 3,
-    titre_question : "IA et législation",
-    url_video : "https://video.univ-rennes.fr/permalink/v126d44fb9ca2m85i6ka/iframe/",
-    url_miniature : "https://picsum.photos/seed/rennes/400/250"
-  }
+  { 
+    id: 3, 
+    titre_question: "IA et législation", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9ca2m85i6ka/iframe/ ", 
+    url_miniature: "https://picsum.photos/seed/rennes/400/250" 
+  },
 
+  { 
+    id: 4, 
+    titre_question: "IA et enseignemnet", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9328lzfp40f/iframe/", 
+    url_miniature: "https://picsum.photos/seed/rennes/400/250" 
+  },
 ];
