6dfdd6ebbdbf189e4386492cc1120677c6f22a8d
Mehdi Munim <mehdi.munim@gmail.com>
2026-05-19 15:28:30 +0200

    chore: fix

--- data/videos.js
+++ data/videos.js
@@ -3,7 +3,7 @@ const videoData = [
    { 
     id: 1, 
     titre_question: "IA vs chercheur.euse", 
-    url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9c10k5dviyx/iframe/?autoplay=true", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d44fb9c10k5dviyx/iframe/?autoplay=1", 
     url_miniature: "assets/img/IA_et_recherche.png",
     duree_seconde: 47
   },
@@ -11,7 +11,7 @@ const videoData = [
   { 
     id: 2, 
     titre_question: "IA et législation", 
-    url_video: "https://video.univ-rennes.fr/videos/ia-et-legislation-vf/iframe/", 
+    url_video: "https://video.univ-rennes.fr/videos/ia-et-legislation-vf/iframe/?autoplay=1", 
     url_miniature: "assets/img/IA_et_droit.png",
     duree_seconde: 200 
   },
@@ -19,7 +19,7 @@ const videoData = [
   { 
     id: 3, 
     titre_question: "IA et enseignement", 
-    url_video: "https://video.univ-rennes.fr/permalink/v126d450ab67bqt2aw1u/iframe/", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d450ab67bqt2aw1u/iframe/?autoplay=1", 
     url_miniature: "assets/img/IA_enseignement.png",
     duree_seconde: 182 
   },
@@ -27,7 +27,7 @@ const videoData = [
   { 
     id: 4, 
     titre_question: "IA et humain", 
-    url_video: "https://video.univ-rennes.fr/permalink/v126d44fc8253dl70bzg/iframe/", 
+    url_video: "https://video.univ-rennes.fr/permalink/v126d44fc8253dl70bzg/iframe/?autoplay=1", 
     url_miniature: "assets/img/IA.png",
     duree_seconde:162 
   },
@@ -35,7 +35,7 @@ const videoData = [
   {
     id: 5,
     titre_question: "IA et environnement",
-    url_video: "https://video.univ-rennes.fr/videos/ia-environnementmusiquemp4/iframe/",
+    url_video: "https://video.univ-rennes.fr/videos/ia-environnementmusiquemp4/iframe/?autoplay=1 ",
     url_miniature: "assets/img/IA_et_environnement.png",
     duree_seconde:386
   },
